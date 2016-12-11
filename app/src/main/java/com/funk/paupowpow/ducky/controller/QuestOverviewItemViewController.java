95995fa9a00f3b9ca7548fa81204be8d359ea980
Paula <ph@dcb.ug>
2016-12-11 18:49:45 +0100

    invokes an Intent to capture a photo with the camera app

--- app/src/main/java/com/funk/paupowpow/ducky/controller/QuestOverviewItemViewController.java
+++ app/src/main/java/com/funk/paupowpow/ducky/controller/QuestOverviewItemViewController.java
@@ -1,8 +1,11 @@
 package com.funk.paupowpow.ducky.controller;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.funk.paupowpow.ducky.R;
@@ -20,8 +23,15 @@ public class QuestOverviewItemViewController {
     private Activity activity;
     private Quest quest;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+
+
     @Bind(R.id.quest_overview_item_text)
     TextView questText;
+
+    @Bind(R.id.quest_overview_item_button)
+    Button snapButton;
 
     public QuestOverviewItemViewController(View itemView, Activity activity) {
         this.itemView = itemView;
@@ -35,6 +45,16 @@ public class QuestOverviewItemViewController {
         this.questText.setText(quest.getQuestText());
 
         Log.d("QUEST", "" + quest.getQuestText());
+
+        snapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+                    activity.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
 
     public void onViewRecycled() {
