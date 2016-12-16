fedb9ca029bdf41db8734f134409029f4863a4f9
Paula <ph@dcb.ug>
2016-12-16 16:06:27 +0100

    created controller for image view in quest detail view

--- app/src/main/java/com/funk/paupowpow/ducky/controller/QuestDetailImageViewController.java
+++ app/src/main/java/com/funk/paupowpow/ducky/controller/QuestDetailImageViewController.java
@@ -1,8 +1,24 @@
 package com.funk.paupowpow.ducky.controller;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by paulahaertel on 16.12.16.
  */
 
 public class QuestDetailImageViewController {
+
+    private Bitmap image;
+
+    public QuestDetailImageViewController(Bitmap image) {
+        this.image = image;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
 }
