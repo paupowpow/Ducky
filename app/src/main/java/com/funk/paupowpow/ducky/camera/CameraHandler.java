edae0f42c8d5fc52e9f8e4b7084444a78ca49c2e
Paula <ph@dcb.ug>
2016-12-18 14:09:01 +0100

    adapted manifest to allow read/write to internal storage

---
+++ app/src/main/java/com/funk/paupowpow/ducky/camera/CameraHandler.java
@@ -0,0 +1,8 @@
+package com.funk.paupowpow.ducky.camera;
+
+/**
+ * Created by paulahaertel on 17.12.16.
+ */
+
+public class CameraHandler {
+}
---
+++ app/src/main/java/com/funk/paupowpow/ducky/model/data/QuestPicture.java
@@ -0,0 +1,9 @@
+package com.funk.paupowpow.ducky.model.data;
+
+/**
+ * Created by paulahaertel on 17.12.16.
+ */
+
+public class QuestPicture {
+
+}
