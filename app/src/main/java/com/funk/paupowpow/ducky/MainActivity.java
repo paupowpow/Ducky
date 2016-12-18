8583cb07bddfff109d2c723dd900d3a67a2cc92b
Paula <ph@dcb.ug>
2016-12-18 14:09:50 +0100

    moved to new camera handler

--- app/src/main/java/com/funk/paupowpow/ducky/MainActivity.java
+++ app/src/main/java/com/funk/paupowpow/ducky/MainActivity.java
@@ -1,17 +1,16 @@
 package com.funk.paupowpow.ducky;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.funk.paupowpow.ducky.camera.CameraHandler;
 import com.funk.paupowpow.ducky.fragments.DuckyFragmentManager;
 import com.funk.paupowpow.ducky.model.data.DuckyDatabaseHandler;
+import com.funk.paupowpow.ducky.model.data.Quest;
 import com.funk.paupowpow.ducky.onboarding.Onboarding;
 
 public class MainActivity extends AppCompatActivity {
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +22,21 @@ public class MainActivity extends AppCompatActivity {
 
         DuckyDatabaseHandler.initialize(this);
         Onboarding.createOnboardingQuest();
+
+        CameraHandler.initialize(this);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap image = (Bitmap) extras.get("data");
-            DuckyFragmentManager.getInstance().startQuestDetailFragment(image);
+
+        Quest questToOpen = CameraHandler.getInstance().handleResult(requestCode, resultCode, data);
+
+        if (questToOpen != null) {
+            DuckyFragmentManager.getInstance().startQuestDetailFragment(questToOpen);
         }
     }
+
+
 
 }
