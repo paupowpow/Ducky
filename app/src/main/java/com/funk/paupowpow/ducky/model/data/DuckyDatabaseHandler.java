48ea2114d7b4a9e2c6b4ff907b30c59949b07b80
Paula <ph@dcb.ug>
2016-12-18 14:16:24 +0100

    minor changes

--- app/src/main/java/com/funk/paupowpow/ducky/model/data/DuckyDatabaseHandler.java
+++ app/src/main/java/com/funk/paupowpow/ducky/model/data/DuckyDatabaseHandler.java
@@ -1,11 +1,20 @@
 package com.funk.paupowpow.ducky.model.data;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
-
 /**
  * Created by paulahaertel on 10.12.16.
  */
@@ -13,13 +22,18 @@ import io.realm.RealmResults;
 public class DuckyDatabaseHandler {
     Realm myRealm;
 
+    String mCurrentPhotoPath;
+
     private static DuckyDatabaseHandler instance;
+    private static Activity activity;
 
     public static void initialize(Activity activity) {
-        if (instance == null) {instance = new DuckyDatabaseHandler(activity); }
+        if (instance == null) {
+            instance = new DuckyDatabaseHandler(activity);
+        }
     }
 
-    private DuckyDatabaseHandler (Activity activity) {
+    private DuckyDatabaseHandler(Activity activity) {
         RealmConfiguration defaultConfiguration = new RealmConfiguration.Builder(activity)
                 .name(Realm.DEFAULT_REALM_NAME)
                 .schemaVersion(0)
@@ -27,6 +41,7 @@ public class DuckyDatabaseHandler {
                 .build();
         Realm.setDefaultConfiguration(defaultConfiguration);
         myRealm = Realm.getInstance(defaultConfiguration);
+        this.activity = activity;
     }
 
     public static DuckyDatabaseHandler getInstance() {
